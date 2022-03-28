7266f24f3c8bbecc5ebc8e5b5add4712f9328c3e
Hug Folguera <hug.f.folguera@oracle.com>
2022-03-28 11:16:21 +0200

    Apply workaround for logger.fatal

--- config/db.config.js
+++ config/db.config.js
@@ -1,6 +1,5 @@
 var mongoose = require('mongoose');
 
-//const logger = require('./logger.config').logger;
 const logger = require('./logger.config').logger;
 
 
@@ -10,7 +9,7 @@ var MONGODB_URL = process.env['MONGODB_URL'];
 // Database connection
 mongoose.connect(MONGODB_URL, { useNewUrlParser: true, useUnifiedTopology: true }, function(err, res) {
     if (err) {
-        logger.fatal('ERROR: connecting to Database. ' + err);
+        logger.info('ERROR: connecting to Database. ' + err); // TODO: Fix logger.fatal is not a function error
     } else {
         logger.info('Connected to Database.');
     }
