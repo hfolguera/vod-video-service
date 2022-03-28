b40ddab86bfa21d146cf2d9ef1c0c63cb4e18815
Hug Folguera <hug.f.folguera@oracle.com>
2022-03-28 09:44:18 +0200

    Remove comments

--- app.js
+++ app.js
@@ -15,33 +15,10 @@ const logger = require('./config/logger.config').logger;
 require('./config/db.config');
 
 // Models
-var models = require('./models/video.model')(app); // Needed?
+var models = require('./models/video.model')(app);
 
 // Routes
 require('./routes/video.routes')(app,express);
-// Controllers
-// var VideoController = require('./controllers/video.controller');
-// var video = express.Router();
-
-// video.route('/video')
-//     .get(VideoController.getVideos)
-//     .post(VideoController.addVideo);
-
-// video.route('/video/:id')
-//     .get(VideoController.getVideoById)
-//     .put(VideoController.updateVideo)
-//     .delete(VideoController.deleteVideo);
-
-// app.use('/api', video);
-
-// var router = express.Router();
-
-// router.get('/', function(req, res) {
-//    res.send("Hello World!");
-// });
-
-// app.use(router);
-
 
 app.listen(2000, function() {
     logger.info("Node server running on http://localhost:2000");
